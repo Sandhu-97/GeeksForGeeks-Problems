2a814470fa2af0ad82605f11905a4076d09e644d
Daanveer Singh <78742845+Sandhu-97@users.noreply.github.com>
2025-07-26 10:01:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
+++ Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
@@ -1,44 +1,42 @@
 class Solution {
   public:
-    vector<string> res;
-    
-    
-    void solve(int i, int j, string& path, vector<vector<int>>& maze){
-        if (i<0 || i>=maze.size() || j<0 || j>=maze.size() || maze[i][j]==0) return;
-        if (i==maze.size()-1 && j==maze.size()-1) {
-            res.push_back(path);
+  
+    void solve(int i, int j, int n, vector<vector<int>>& maze, string& path, vector<string>& ans){
+        if (i==n-1 && j==n-1){
+            ans.push_back(path);
             return;
         }
+        if (i<0 || j<0 || i>=n || j>=n || maze[i][j]!=1) return;
         
-        maze[i][j] = 0;
         
+        maze[i][j]=0;
         path.push_back('D');
-        solve(i+1, j, path, maze);
+        solve(i+1, j, n, maze, path, ans);
         path.pop_back();
         
         path.push_back('L');
-        solve(i, j-1, path, maze);
+        solve(i, j-1, n, maze, path, ans);
         path.pop_back();
         
         path.push_back('R');
-        solve(i, j+1, path, maze);
+        solve(i, j+1, n, maze, path, ans);
         path.pop_back();
         
         path.push_back('U');
-        solve(i-1, j, path, maze);
+        solve(i-1, j, n, maze, path, ans);
         path.pop_back();
         
-        
         maze[i][j]=1;
-        
-        
         
     }
     vector<string> ratInMaze(vector<vector<int>>& maze) {
         // code here
-        string path="";
-        solve(0,0,path,maze);
-        return res;
+        string path = "";
+        vector<string> ans;
+        
+        solve(0,0,maze.size(),maze,path,ans);
+        return ans;
+        
         
     }
 };
